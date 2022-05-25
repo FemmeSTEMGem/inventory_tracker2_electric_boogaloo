cd98874a316f8ca5eaf9f0b516e60fff3b6a06a3
FemmeSTEMGem <femmestemgem@gmail.com>
2022-05-25 16:41:40 -0400

    Updating routes to include create/destroy/move inventory

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,13 @@ Rails.application.routes.draw do
   resources :locations
   resources :items
   resources :item_locations, only: [ :edit, :update ]
+
+  get 'create_inventory', to: 'item_locations#create_inventory'
+  post 'create_inventory_action', to: 'item_locations#create_inventory_action'
+
+  get 'destroy_inventory', to: 'item_locations#destroy_inventory'
+  post 'destroy_inventory_action', to: 'item_locations#destroy_inventory_action'
+  
+  get 'move_inventory', to: 'item_locations#move_inventory'
+  post 'move_inventory_action', to: 'item_locations#move_inventory_action'
 end
