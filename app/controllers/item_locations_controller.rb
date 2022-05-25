0ed0578d138c8747d2c37ebac1042a4d376d73e5
FemmeSTEMGem <femmestemgem@gmail.com>
2022-05-25 16:38:29 -0400

    Completing ItemLocations controller

--- app/controllers/item_locations_controller.rb
+++ app/controllers/item_locations_controller.rb
@@ -1,2 +1,44 @@
 class ItemLocationsController < ApplicationController
+
+  def create_inventory
+    @item_names = Item.all.pluck(:name)
+    @location_names = Location.all.pluck(:name)
+  end
+
+  def destroy_inventory
+    @item_location = ItemLocation.find_by(
+      item: Item.find_by(name: params[:item]),
+      location: Location.find_by(name: params[:location])
+    ) do |item_location|
+      item_location.update(quantity: item_location.quantity -= params[:quantity])
+    end
+
+    if @item_location.save
+      redirect_to root_path
+    end
+  end
+
+  def move_inventory
+
+  end
+
+  def create_inventory_action
+    @item_location = ItemLocation.new(
+      item: Item.find_by(name: params[:item]),
+      location: Location.find_by(name: params[:location]),
+      quantity: params[:quantity]
+    )
+
+    if @item_location.save
+      redirect_to root_path
+    end
+  end
+
+  def destroy_inventory_action
+
+  end
+
+  def move_inventory_action
+
+  end
 end
